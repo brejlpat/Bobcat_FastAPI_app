5378262190e1826974db2faa52e432b288228b3b
brejlpat <patrikbrejla@seznam.cz>
2025-05-26 07:32:05 +0200

    Docker init

--- init_db/init.sql
+++ init_db/init.sql
@@ -10,3 +10,20 @@ CREATE TABLE IF NOT EXISTS users_ad (
 INSERT INTO users_ad (email, role)
 VALUES ('patrik.brejla@doosan.com', 'admin')
 ON CONFLICT DO NOTHING;
+
+CREATE TABLE IF NOT EXISTS login (
+    id SERIAL PRIMARY KEY,
+    username TEXT,
+    token_expiration TEXT,
+    login_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS device_edit (
+    id SERIAL PRIMARY KEY,
+    username TEXT,
+    channel_name TEXT,
+    payload JSONB,
+    device_edit_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    action TEXT,
+    driver TEXT
+)
